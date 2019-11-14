4ce7287f56118ae0e7bce4fa7e48a699861c7d17
cminnelli <cminnelli@gmail.com>
2019-11-14 17:54:01 -0300

    ferias

--- public/js/controllers/chacinadoController.js
+++ public/js/controllers/chacinadoController.js
@@ -6,7 +6,8 @@ app.controller("chacinadoController" , function($scope , $http , googleService ,
 	$scope.categoriasEleccion = [];
 	$scope.backCategory = "que"
 
-	window.onscroll = function(){
+	$scope.activateProd = function(){
+
 		var h = window.screen.height;
 		var y = window.scrollY;
 		console.log(y);
@@ -21,7 +22,11 @@ app.controller("chacinadoController" , function($scope , $http , googleService ,
 			$(".categoriaModulo").removeClass("myFix");
 			$(".productoModulo").removeClass("myMargin")
 			$(".titulosmain").show(1000); 
-		}
+		}		
+	}
+
+	window.onscroll = function(){
+		$scope.activateProd()
 	}
 
 	var timeInit = 0.2;
@@ -32,16 +37,15 @@ app.controller("chacinadoController" , function($scope , $http , googleService ,
 	{nombre:"Frutos" , catId:"fru" , img:"https://images.pexels.com/photos/821365/pexels-photo-821365.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"},
 	{nombre:"Extras" , catId:"ext" , img:"https://images.pexels.com/photos/821365/pexels-photo-821365.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"},
 	{nombre:"Oliva" , catId:"oli" , img:"https://images.pexels.com/photos/821365/pexels-photo-821365.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"}
+	]
 
+	$scope.direcciones = [{lugar: "Plaza de las Carretas" , localidad: "Muñiz, San Miguel", direccion:"Av. Presidente peron esquina San José" , horario:"Sábado y Domingo 10:00hs-21:00hs" , mapUrl:"https://goo.gl/maps/N9RMAkzQpbtkMdTHA"},
+	{lugar: "Parque Palacio municipal " , localidad:"Malvinas Argentinas" , direccion:"Av Presidente. Juan Domingo Peron 4276" , horario:"Sábado y Domingo 10:00hs-21:00hs" , mapUrl:"https://goo.gl/maps/N9RMAkzQpbtkMdTHA"}
 	]
 
 	$scope.productos = [
 		{nombre:"Guda" , marca:"Sancor",  um:"10kg" , color:"Beige",  cat:"Quesos" , catId:"que" , des:"Queso de cabra y con un tinte beige, excelente gust" , img:"https://previews.123rf.com/images/simicv/simicv1809/simicv180900035/109202324-sliced-fresh-emmental-cheese-on-white-background-cow-cheese.jpg" , celiaco:true },
 		{nombre:"Mardel" , marca:"Veronica",  um:"13kg" , color:"Beige",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
-		{nombre:"Regianatto" , marca:"Sancor",  um:"13kg" , color:"Purpura",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
-		{nombre:"Regianatto" , marca:"Sancor",  um:"13kg" , color:"Purpura",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
-		{nombre:"Regianatto" , marca:"Sancor",  um:"13kg" , color:"Purpura",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
-		{nombre:"Regianatto" , marca:"Sancor",  um:"13kg" , color:"Purpura",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
 		{nombre:"Regianatto" , marca:"Sancor",  um:"13kg" , color:"Purpura",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
 		{nombre:"Regianatto" , marca:"Sancor",  um:"13kg" , color:"Purpura",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
 		{nombre:"test" , marca:"Sancor",  um:"13kg" , color:"Purpura",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
@@ -52,9 +56,9 @@ app.controller("chacinadoController" , function($scope , $http , googleService ,
 		{nombre:"Nuez" , marca:"grandax",   um:"1l" , color:"Marron" ,  cat:"Frutos secos" , catId:"fru" , des:"Vino de calidad, mendocino de cepa" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
 		{nombre:"Avellana" , marca:"grandax",   um:"120" , color:"Marron" ,  cat:"Frutos secos" , catId:"fru" , des:"Vino de calidad, mendocino de cepa" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
 		{nombre:"Oliva" , marca:"El Sol",   um:"120" , color:"verde" ,  cat:"Aceitunas" , catId:"oli" , des:"aceite de oliva sin igual" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
-				{nombre:"Oliva" , marca:"El Sol",   um:"120" , color:"verde" ,  cat:"Aceitunas" , catId:"oli" , des:"aceite de oliva sin igual" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
-						{nombre:"Oliva" , marca:"El Sol",   um:"120" , color:"verde" ,  cat:"Aceitunas" , catId:"oli" , des:"aceite de oliva sin igual" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
-								{nombre:"Oliva" , marca:"El Sol",   um:"120" , color:"verde" ,  cat:"Aceitunas" , catId:"oli" , des:"aceite de oliva sin igual" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
+		{nombre:"Oliva" , marca:"El Sol",   um:"120" , color:"verde" ,  cat:"Aceitunas" , catId:"oli" , des:"aceite de oliva sin igual" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
+		{nombre:"Oliva" , marca:"El Sol",   um:"120" , color:"verde" ,  cat:"Aceitunas" , catId:"oli" , des:"aceite de oliva sin igual" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
+		{nombre:"Oliva" , marca:"El Sol",   um:"120" , color:"verde" ,  cat:"Aceitunas" , catId:"oli" , des:"aceite de oliva sin igual" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
 		{nombre:"Aceituna" , marca:"El Sol",   um:"120" , color:"verde" ,  cat:"Extras" , catId:"ext" , des:"Las mejores aceitunas del condado" , celiaco:true , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
 	]
 	$scope.init = function(){
@@ -64,11 +68,19 @@ app.controller("chacinadoController" , function($scope , $http , googleService ,
 
 	}
 
-	$scope.whatsapp = function(txt){
-		if(txt == ""){
+	$scope.whatsapp = function(txt , tp){
+		var msg;
+
+		if(txt == undefined){
 			alert("Insertar nombre");
 		}else{
-			var msg = "Hola, soy "+ txt + " me interesaria saber más acerca como emprender en Chacinados el Condor!"
+			if(tp == "new"){
+				msg = "Hola! Quisiera que agreguen mi mail "+ txt + " al newsletter de Chacinados el Condor!"
+
+			}else{
+				msg = "Hola, soy "+ txt + " me interesaria saber más acerca como emprender en Chacinados el Condor!"
+			}
+
 			window.location = "https://api.whatsapp.com/send?phone=5491136486047&text=" + msg;
 		}
 	}
