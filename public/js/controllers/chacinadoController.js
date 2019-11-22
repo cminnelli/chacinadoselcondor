4700a29c74c03228ff841d9b99195c3ea8387313
cminnelli <cminnelli@gmail.com>
2019-11-22 12:59:17 -0300

    correccion productos

--- public/js/controllers/chacinadoController.js
+++ public/js/controllers/chacinadoController.js
@@ -32,18 +32,18 @@ app.controller("chacinadoController" , function($scope , $http , googleService ,
 
 		var h = window.screen.height;
 		var y = window.scrollY;
-		console.log(y);
+		// console.log(y);
 		if (y > (h*2) && y < (h*3) -120){
 			console.log("arranca")
-			$(".categoriaModulo").addClass("myFix")
-			$(".productoModulo").addClass("myMargin")
+			// $(".categoriaModulo").addClass("myFix")
+			// $(".productoModulo").addClass("myMargin")
 			$(".menuIcon").fadeOut(400); 
 
 
 		}else{
 			// $(".categoriaModulo").height("0px");
-			$(".categoriaModulo").removeClass("myFix");
-			$(".productoModulo").removeClass("myMargin")
+			// $(".categoriaModulo").removeClass("myFix");
+			// $(".productoModulo").removeClass("myMargin")
 			$(".menuIcon").fadeIn(400); 
 		}		
 	}
@@ -69,17 +69,17 @@ app.controller("chacinadoController" , function($scope , $http , googleService ,
 	]
 
 	$scope.productos = [
-		{nombre:"Guda" , marca:"Sancor",  um:"10kg" , color:"Beige",  cat:"Quesos" , catId:"que" , des:"Queso de cabra y con un tinte beige, excelente gust" , img:"https://previews.123rf.com/images/simicv/simicv1809/simicv180900035/109202324-sliced-fresh-emmental-cheese-on-white-background-cow-cheese.jpg" , celiaco:true },
-		{nombre:"Mardel" , marca:"Veronica",  um:"13kg" , color:"Beige",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
-		{nombre:"Regianatto" , marca:"Sancor",  um:"13kg" , color:"Purpura",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
-		{nombre:"Regianatto" , marca:"Sancor",  um:"13kg" , color:"Purpura",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
-		{nombre:"test" , marca:"Sancor",  um:"13kg" , color:"Purpura",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
-		{nombre:"Salame" , marca:"ferragio",   um:"10kg" , color:"Rosa" ,  cat:"Quesos" , catId:"emb" , des:"lorem	ipsum ipsum ipsum lorem lorem test",  celiaco:true , sal:true , img:"https://thumbs.dreamstime.com/z/different-sorts-cheese-white-background-45703789.jpg"},
-		{nombre:"Jamon Serrano" , marca:"Paladini",   um:"10kg" , color:"Rosado" ,  catId:"emb" , cat:"Quesos" , des:"Jamon de cabra y ..", celiaco:true , img:"https://www.consumer.es/wp-content/uploads/2019/07/img_lonchas-pavo-pollo-jamon-hd.jpg"},
+		{nombre:"Guda" , marca:"Sancor",  um:"5" , color:"Beige",  cat:"Quesos" , catId:"que" , des:"Queso de cabra y con un tinte beige, excelente gust" , img:"https://previews.123rf.com/images/simicv/simicv1809/simicv180900035/109202324-sliced-fresh-emmental-cheese-on-white-background-cow-cheese.jpg" , celiaco:true },
+		{nombre:"Mardel" , marca:"Veronica",  um:"6" , color:"Beige",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
+		{nombre:"Regianatto" , marca:"Sancor",  um:"6" , color:"Purpura",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
+		{nombre:"Regianatto" , marca:"Sancor",  um:"6" , color:"Purpura",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
+		{nombre:"test" , marca:"Sancor",  um:"6" , color:"Purpura",  cat:"Quesos" , catId:"que" , des:"Degustar este queso, es el mejor queso de todos" ,  celiaco:false , sal:true ,  img:"https://tcs.imgix.net/2015/06/GOUDA-CUMIN-STACKED-TOP.jpg?auto=compress%2Cformat&ixlib=php-1.2.1"},
+		{nombre:"Salame" , marca:"ferragio",   um:"3" , color:"Rosa" ,  cat:"Quesos" , catId:"emb" , des:"lorem	ipsum ipsum ipsum lorem lorem test",  celiaco:true , sal:true , img:"https://thumbs.dreamstime.com/z/different-sorts-cheese-white-background-45703789.jpg"},
+		{nombre:"Jamon Serrano" , marca:"Paladini",   um:"3" , color:"Rosado" ,  catId:"emb" , cat:"Quesos" , des:"Jamon de cabra y ..", celiaco:true , img:"https://www.consumer.es/wp-content/uploads/2019/07/img_lonchas-pavo-pollo-jamon-hd.jpg"},
 		{nombre:"Mendocino" , marca:"Malbec",   um:"1l" , color:"te" , catId:"vin" , cat:"Vinos y bebidas"  , des:"Queso de cabra y ..", celiaco:true , sal:true , img:"https://soloporgusto.com/wp-content/uploads/2018/06/gran-enemigo-Catena-Zapata-Adrianna.jpg"},
 		{nombre:"Nocturno" , marca:"Mendocino",   um:"1l" , color:"te" ,  cat:"Vinos y bebidas" , catId:"vin" , des:"Vino de calidad, mendocino de cepa" , celiaco:false , sal:true , img:"https://cdn.shopify.com/s/files/1/0005/4634/0925/products/marlodulce_2048x2048.jpg?v=1556299447"},
-		{nombre:"Nuez" , marca:"grandax",   um:"1l" , color:"Marron" ,  cat:"Frutos secos" , catId:"fru" , des:"Vino de calidad, mendocino de cepa" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
-		{nombre:"Avellana" , marca:"grandax",   um:"120" , color:"Marron" ,  cat:"Frutos secos" , catId:"fru" , des:"Vino de calidad, mendocino de cepa" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
+		{nombre:"Nuez" , marca:"grandax",   um:"" , color:"Marron" ,  cat:"Frutos secos" , catId:"fru" , des:"Vino de calidad, mendocino de cepa" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
+		{nombre:"Avellana" , marca:"grandax",   um:"" , color:"Marron" ,  cat:"Frutos secos" , catId:"fru" , des:"Vino de calidad, mendocino de cepa" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
 		{nombre:"Oliva" , marca:"El Sol",   um:"120" , color:"verde" ,  cat:"Aceitunas" , catId:"oli" , des:"aceite de oliva sin igual" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
 		{nombre:"Oliva" , marca:"El Sol",   um:"120" , color:"verde" ,  cat:"Aceitunas" , catId:"oli" , des:"aceite de oliva sin igual" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
 		{nombre:"Oliva" , marca:"El Sol",   um:"120" , color:"verde" ,  cat:"Aceitunas" , catId:"oli" , des:"aceite de oliva sin igual" , celiaco:false , sal:true , img:"https://www.frutosare.com.ar/wp-content/uploads/2017/04/nuez-con-cascara.jpg"},
@@ -93,6 +93,13 @@ app.controller("chacinadoController" , function($scope , $http , googleService ,
 
 	}
 
+	$scope.umSeach = function(ct){
+		if (ct == "vin" || ct =="emb"){
+			return "litros"
+		}else if(ct == "que"){
+			return "kilos"
+		}
+	}
 	$scope.whatsapp = function(txt , tp){
 		var msg;
 
@@ -140,6 +147,8 @@ app.controller("chacinadoController" , function($scope , $http , googleService ,
 			$("svg").eq(ind).fadeOut(400)
 		},3500)
 
+
+		$("svg").eq(ind).fadeIn(400);
 		// alert(ind)
 		console.log(cat)
 		console.log($scope.categoriasEleccion)
